c6395935138b17a326a97128ac8dc91eb9c2b7d8
bsatoriu <brian.p.satorius@jpl.nasa.gov>
2018-09-26 16:26:37 -0700

    Update Catify.py

--- Catify.py
+++ Catify.py
@@ -6,22 +6,33 @@ import sys
 def main():
 
     img_base = 'input.jpg'
-    img_overlay = sys.argv[1:][0]
-    img_composite = 'output.jpg'
-    image_url = sys.argv[1:][1]
-    img_data = requests.get(image_url).content
+    image_base_url = sys.argv[1:][1]
+    img_base_data = requests.get(image_base_url).content
     with open(img_base, 'wb') as handler:
-        handler.write(img_data)
+        handler.write(img_base_data)
 
     # Relative Path
     # Image on which we want to paste
     img = Image.open(img_base)
 
+    
+    
+    
+    img_overlay = 'overlay.jpg'
+    img_overlay_url = sys.argv[1:][0]
+    img_overlay_data = requests.get(img_overlay_url).content
+    with open(img_overlay, 'wb') as handler:
+        handler.write(img_overlay_data)
+    
+    
+    
+    
     # Relative Path
     # Image which we want to paste
     img2 = Image.open(img_overlay)
     img.paste(img2, (50, 50))
 
+    img_composite = 'output.jpg'
     # Saved in the same relative location
     img.save(img_composite)
 
