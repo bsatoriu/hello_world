3a47db446d470190b253211296ff9490b975edcf
bsatoriu <brian.p.satorius@jpl.nasa.gov>
2018-09-26 11:03:47 -0700

    Update run_hello_world.sh

--- run_hello_world.sh
+++ run_hello_world.sh
@@ -2,6 +2,8 @@
 echo "Hello World!"
 echo "Hello World to STDERR" 1>&2
 
+BASE_PATH=$(dirname "${BASH_SOURCE}")
+
 # generate dataset ID
 timestamp=$(date -u +%Y%m%dT%H%M%S.%NZ)
 hash=$(echo $timestamp | sha224sum | cut -c1-5)
@@ -22,3 +24,5 @@ echo "{\"version\": \"v1.0\"}" > $dataset_json_file
 # create minimal metadata file
 metadata_json_file=${id}/${id}.met.json
 echo "{}" > $metadata_json_file
+
+python $BASE_PATH/Catify.py
